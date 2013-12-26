68eca509137f39639b7a94999ea4b6acfff2ec44
Ricard Torres <torres.rick@gmail.com>
2013-12-26 16:49:46 +0100

    Node.js!
    The materials update is done with Node.js, wooo!

--- js/app.js
+++ js/app.js
@@ -1,5 +1,34 @@
-angular.module('GangstersApp', ['ngRoute'])
+angular.module('GangstersApp', ['ngRoute', 'firebase'])
 
-	.run( function($rootScope){
-		$rootScope.appVersion = '0.1';	
+	// .run( function($rootScope){
+	// 	$rootScope.appVersion = '0.1';	
+	// })
+
+	.value('fbURL', 'https://gangsters.firebaseio.com/')
+ 
+	.factory('Users', function($firebase, fbURL) {
+	  return $firebase(new Firebase(fbURL));
+	})
+
+	.controller('MainCtrl', function($scope, $firebase, fbURL){
+
+
+		var ref = new Firebase(fbURL);
+		
+		$scope.users = $firebase(ref);
+
+		// Simple Login
+
+		var auth = new FirebaseSimpleLogin(ref, function(error, user) {
+		  if (error) {
+		    // an error occurred while attempting login
+		    console.log(error);
+		  } else if (user) {
+		    // user authenticated with Firebase
+		    console.log('User ID: ' + user.id + ', Provider: ' + user.provider);
+		  } else {
+		    // user is logged out
+		  }
+		});
+
 	});
---
+++ node/app.js
@@ -0,0 +1,24 @@
+var Firebase = require('firebase');
+
+
+	// I've tried to loop through different users but something weird happens with Node.js and Firebase. I think there's an issue with the non-blocking code, it goes through before it can get the data. Need to look into it.
+	
+	// For now I'll hardcode just the 2 users
+	
+	function addMaterials(){
+	
+			var Ricard = new Firebase('https://gangsters.firebaseio.com/0');			
+			
+			Ricard.once('value', function(data) {
+			 	Ricard.update({alcohol : data.val().alcohol + 10,guns : data.val().guns + 10,money : data.val().money + 10});
+			});
+			
+			var Felman = new Firebase('https://gangsters.firebaseio.com/1');			
+			
+			Felman.once('value', function(data) {
+			 	Felman.update({alcohol : data.val().alcohol + 10,guns : data.val().guns + 10,money : data.val().money + 10});
+			});
+	}
+	
+	
+	var intervalID = setInterval(addMaterials, 30000);
