2f602bfd5ab3cb293b8b5f6d9bbdc985f33c8afa
Ricard Torres <torres.rick@gmail.com>
2013-12-27 20:06:41 +0100

    You can now upgrade the buildings
    The buildings can be upgraded if you have enough materials.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 angular.module('GangstersApp', ['ngRoute', 'firebase'])
 
 	 .run( function($rootScope){
-	 	$rootScope.appVersion = '0.1';	
+	 	$rootScope.appVersion = '0.1';
+	 	$rootScope.laundryCost = 100;	
+	 	$rootScope.distilleryCost = 200;	
+	 	$rootScope.smugglerCost = 300;	
 	 })
 
 	.value('fbURL', 'https://gangsters.firebaseio.com/')
@@ -11,7 +14,7 @@ angular.module('GangstersApp', ['ngRoute', 'firebase'])
 	})
 
 	.config(['$locationProvider', function($locationProvider){
-            $locationProvider.html5Mode(true); // Removed the #  Hashbang mode
+            $locationProvider.html5Mode(false); // Removed the #  Hashbang mode
     }])
 
     // Build up the routes
@@ -44,7 +47,7 @@ angular.module('GangstersApp', ['ngRoute', 'firebase'])
 
 	 })
 
-	 .controller('MenuCtrl', function($scope, $firebase, fbURL){
+	 .controller('MenuCtrl', function($rootScope, $scope, $firebase, fbURL){
 
 	 	$scope.$watch('currentUser', function(newVal, oldVal, scope) { // the newVal of the full_name will be available here
 			
@@ -54,12 +57,7 @@ angular.module('GangstersApp', ['ngRoute', 'firebase'])
 
 				var ref = new Firebase(fbURL + "/" + newVal);
 		
-				$scope.me = $firebase(ref);
-
-				var data = $scope.me;
-
-				var buildings = data.buildings;
-				console.log(buildings);
+				$rootScope.me = $firebase(ref);
 			}
 		});
 	 })
@@ -74,8 +72,67 @@ angular.module('GangstersApp', ['ngRoute', 'firebase'])
 		
 	})
 
-	.controller('BuildingsCtrl', function($scope){
+	.controller('BuildingsCtrl', function($rootScope, $scope, $firebase, fbURL){
 
+		$scope.updateBuilding = function(building, scope){
+
+			// I guess we could improve this to avoid 3 hardcoded if statements, will look later
+
+			if( building == 'laundry' ){
+
+				// Does the user has enough money?
+
+				if( $rootScope.laundryCost*$rootScope.me.buildings.laundry < $rootScope.me.money ){
+
+					// Alrigt, update it and upgrade the building
+
+					$rootScope.me.money = $rootScope.me.money - ($rootScope.laundryCost*$rootScope.me.buildings.laundry);
+					$rootScope.me.buildings.laundry = $rootScope.me.buildings.laundry + 1;
+
+				}
+			}
+
+			if( building == 'distillery' ){
+
+				// Does the user has enough money?
+
+				if( $rootScope.distilleryCost*$rootScope.me.buildings.distillery < $rootScope.me.alcohol ){
+
+					// Alrigt, update it and upgrade the building
+
+					$rootScope.me.alcohol = $rootScope.me.alcohol - ($rootScope.distilleryCost*$rootScope.me.buildings.distillery);
+					$rootScope.me.buildings.distillery = $rootScope.me.buildings.distillery + 1;
+
+				}
+			}
+
+			if( building == 'smuggler' ){
+
+				// Does the user has enough money?
+
+				if( $rootScope.smugglerCost*$rootScope.me.buildings.smuggler < $rootScope.me.guns ){
+
+					// Alrigt, update it and upgrade the building
+
+					$rootScope.me.guns = $rootScope.me.guns - ($rootScope.smugglerCost*$rootScope.me.buildings.smuggler);
+					$rootScope.me.buildings.smuggler = $rootScope.me.buildings.smuggler + 1;
+
+				}
+			}
+
+			var ref = new Firebase(fbURL + "/" + $rootScope.me.id);
+
+			/* There's an issue here. How can I update the jSON object without losing data? 
+				On my first test I just updated like this:  buildings: {laundry:123}  
+				This destroyed distillery and smuggler. It doesn't seem right that I have to re-send 
+				the other two values.
+
+				Will do it for now for the sake of the example so I can move forward.
+			*/
+
+			ref.update({money : $rootScope.me.money, guns : $rootScope.me.guns, alcohol : $rootScope.me.alcohol, buildings :{ laundry: $rootScope.me.buildings.laundry, distillery: $rootScope.me.buildings.distillery, smuggler: $rootScope.me.buildings.smuggler }});
+			
+		};
 	})
 
 	.controller('MeCtrl', function($scope){
